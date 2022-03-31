db0e27c954ad57eb20c06bd3933a56d84c285d37
duwon1229 <44364759+duwon1229@users.noreply.github.com>
2022-03-31 23:14:32 +0900

    apollo graphQL 기본 사용법

---
+++ src/routes/Home.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import styled from "styled-components";
+import Movie from "../components/Movie";
+
+const GET_MOVIEW = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Cover = styled.div``;
+
+const Header = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  height: 300px;
+  padding-top: 100px;
+  background-color: #ff7979;
+  color: #fff;
+  text-align: center;
+  .title {
+    margin: 0;
+    font-size: 40px;
+    font-weight: 700;
+  }
+  .sub {
+    margin: 24px 0 0;
+    font-size: 18px;
+  }
+`;
+
+const Loading = styled.p`
+  text-align: center;
+  margin: 24px 0 0;
+`;
+
+export default () => {
+  const { loading, data } = useQuery(GET_MOVIEW);
+
+  return (
+    <Cover>
+      <Header>
+        <p className="title">Apollo 2020</p>
+        <p className="sub">I love GraphQL</p>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      {!loading &&
+        data.movies &&
+        data.movies.map((m) => <Movie key={m.id} id={m.id} />)}
+    </Cover>
+  );
+};
